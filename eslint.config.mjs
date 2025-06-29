d5f6c80fed116922edce6751e9238ba314ee8315
Daiki Sudo <daiksud@proton.me>
2025-06-29 22:14:18 +0900

    style: format with prettier and vscode

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
 import eslintPluginAstro from 'eslint-plugin-astro'
-const eslintConfig = [...eslintPluginAstro.configs.recommended]
+const eslintConfig = [
+  ...eslintPluginAstro.configs['recommended'],
+  ...eslintPluginAstro.configs['jsx-a11y-recommended'],
+]
 export default eslintConfig
