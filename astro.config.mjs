9195dd1219940f4bf213612b73256061a44305a8
Daiki Sudo <daiksud@proton.me>
2025-07-04 17:15:55 +0900

    feat: add prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import { defineConfig } from 'astro/config'
 export default defineConfig({
   site: 'https://daiksud.com',
   integrations: [sitemap()],
+  prefetch: true,
   trailingSlash: 'always',
   image: {
     domains: ['github.com'],
